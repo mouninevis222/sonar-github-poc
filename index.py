9120a899d355d85e1fefbad25624bc1c86cc2058
mouninevis222 <64479990+mouninevis222@users.noreply.github.com>
2021-04-30 19:30:14 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,12 @@
+import json
+import datetime
+
+
+def handler(event, context):
+    data = {
+        'output': 'Helloworld!!! 15-April-21',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
